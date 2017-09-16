abc670f6e1c266ad5a51f5388102ba6726bf225c
aka-rob <rmizell@gmail.com>
2017-09-16 08:26:58 -0700

    Checkpoint 3 complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocJames', []);
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/teamplates/album.html'
+      });
+  }
+
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})
